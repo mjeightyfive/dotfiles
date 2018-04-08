af1baaf14c9afc5931a5e0ffbe378b7ab808e0de
mjeightyfive <mjeightyfive@gmail.com>
2018-04-08 19:12:09 +0100

    add some init stuff

---
+++ init/react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './App.scss';
+
+const App = () => {
+    return (
+        <h1>Hello World</h1>
+    );
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('app'));
