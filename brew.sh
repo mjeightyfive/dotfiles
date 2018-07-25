3044f290a766880e55361e4c20d5032a862a45c9
mjeightyfive <mjeightyfive@gmail.com>
2018-07-25 15:03:53 +0100

    add brew webp

--- brew.sh
+++ brew.sh
@@ -104,6 +104,7 @@ brew install dark-mode
 #brew install exiv2
 # brew install git
 brew install git-lfs
+brew install webp
 brew install imagemagick --with-webp
 # brew install lua
 # brew install lynx
