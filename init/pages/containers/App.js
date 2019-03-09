b5c76699094e882297b0dee5ed78fcc919d3f826
mjeightyfive <mjeightyfive@gmail.com>
2019-03-09 14:35:40 +0000

    nextjs

---
+++ init/pages/containers/App.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+import './App.scss';
+
+class App extends PureComponent {
+    render() {
+        return (
+            <main id="app">
+                <h1>Hello World</h1>
+            </main>
+        );
+    }
+}
+
+export default App;
