0c7b35b514324a5eccb94a0117f083e2429b81b8
mjeightyfive <mjeightyfive@gmail.com>
2019-07-27 12:51:04 +0100

    update react boilerplate

--- init/react/src/App.js
+++ init/react/src/App.js
@@ -1,14 +1,11 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
 import './App.scss';
 
-class App extends PureComponent {
-    render() {
-        return <h1>Hello World</h1>;
-    }
-}
-
+const App = () => {
+    return <h1>Hello World</h1>;
+};
 export default App;
 
 render(<App />, document.getElementById('app'));
