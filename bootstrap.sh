9e044c434796b59f04eed7cfcf59f9ff19bd8457
Marcin <mjeightyfive@gmail.com>
2022-02-19 12:38:40 +0000

    use zprofile

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,8 +11,9 @@ function doIt() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
+        --exclude "configs" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
+	source ~/.z_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
